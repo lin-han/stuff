7114b733e309cbc90b857b5f6dec825c4f867520
yingh97 <yingh97@gmail.com>
2017-04-06 20:53:46 -0400

    forgot to change hash.h

--- a4/hash.h
+++ a4/hash.h
@@ -4,7 +4,7 @@
 #define BLOCKSIZE 8
 
 // Hash manipulation helper functions
-char *hash(FILE *f);
+char *hash(char *hash_val, FILE *f);
 int check_hash(const char *hash1, const char *hash2);
 
 #endif // _HASH_H_
